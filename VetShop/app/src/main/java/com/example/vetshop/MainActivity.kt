48b1818cdf6311211ce74a7e09d0ba710db60014
TheHaise <jordan.zelayahs@gmail.com>
2024-04-17 18:45:35 -0600

    Added Menu doctor

--- VetShop/app/src/main/java/com/example/vetshop/MainActivity.kt
+++ VetShop/app/src/main/java/com/example/vetshop/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val loginButton: Button = findViewById(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            // Mostrar un mensaje de bienvenida
+            // Snackbar.make(it, "Bienvenido", Snackbar.LENGTH_SHORT).show()
+            // Crear un Intent para abrir la actividad HolaActivity
+            val intent = Intent(this, Menu::class.java)
+            // Iniciar la actividad HolaActivity
+            startActivity(intent)
+        }
+
     }
 }
