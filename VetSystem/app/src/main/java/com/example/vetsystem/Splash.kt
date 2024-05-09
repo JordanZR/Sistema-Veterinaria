5f49fbbf2cd557306023b4ff6d010501f3faf3f3
TheHaise <jordan.zelayahs@gmail.com>
2024-05-08 19:20:40 -0600

    Added splash

---
+++ VetSystem/app/src/main/java/com/example/vetsystem/Splash.kt
@@ -0,0 +1,28 @@
+package com.example.vetsystem
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Espera 5 segundos y luego inicia la actividad principal
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000) // 5000 milisegundos = 5 segundos
+    }
+}
